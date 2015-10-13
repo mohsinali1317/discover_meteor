4a66e3f992f17582a9d27047a6a713b78afc3e4c
Chaudhry Mohsin Ali <mohsinali1017@gmail.com>
2015-10-13 11:58:41 +0200

    Done with templates. Added new templates and template helpers

---
+++ discover_meteor/client/views/posts/post_item.js
@@ -0,0 +1,11 @@
+/**
+ * Created by mohsin on 10/13/2015.
+ */
+
+Template.postItem.helpers({
+    domain: function () {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+});
