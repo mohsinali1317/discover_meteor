6352dd1b78c64572d25825485862cb7bfbf59335
Chaudhry Mohsin Ali <mohsinali1017@gmail.com>
2015-10-14 09:32:40 +0200

    creating post done.

--- discover_meteor/lib/router.js
+++ discover_meteor/lib/router.js
@@ -8,9 +8,27 @@ Router.configure({
 });
 Router.map(function() {
     this.route('postsList', {path: '/'});
+
     this.route('postPage', {
         path: '/posts/:_id',
         data: function() { return Posts.findOne(this.params._id); }
     });
+
+    this.route('postSubmit', {
+        path: '/submit'
+    });
 });
+
+var requireLogin = function() {
+    if (! Meteor.user()) {
+        if (Meteor.loggingIn())
+            this.render(this.loadingTemplate);
+        else
+            this.render('accessDenied');
+    } else {
+        this.next();
+    }
+}
+
 Router.onBeforeAction('loading');
+Router.onBeforeAction(requireLogin, {only: 'postSubmit'});
